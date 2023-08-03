c9225f0afaa5bb636d60076186a6ad764c45eff7
Arjamand Ali <140328490+arjamand@users.noreply.github.com>
2023-08-03 06:38:16 -0700

    Update 001-input-output.cpp

---
+++ 002-input-output/001-input-output.cpp
@@ -0,0 +1,14 @@
+// 2023-08-03 06:19:46
+
+#include <iostream>
+using namespace std ;
+
+int main(){
+    int num1 ,num2 ;
+    cout << "Adding Two Integers"<<endl<<"Input the first Integer : ";
+    cin >> num1;
+    cout <<"Input the Second Integer : ";
+    cin >> num2;
+    cout <<"The Sum of "<<num1<<" and"<<num2<<" is : "<<num1+num2;
+    return 0 ;
+}
