c87cd13c06c794032f78aeea6221eb2318a41aa7
Arjamand Ali <140328490+arjamand@users.noreply.github.com>
2023-08-05 12:27:19 -0700

    Create 001-basic-code-structure-cpp.cpp

---
+++ 000-intro-to-cpp/001-basic-code-structure-cpp.cpp
@@ -0,0 +1,26 @@
+// 2023-08-05 12:22:40
+
+#include <iostream> // Example of a header file inclusion
+
+int main() {
+    // Body of the program
+    std::cout << "Hii, Welcome to AKHSS,Gilgit ." << std::endl; // Output a message
+    return 0; // Terminate the program
+}
+
+
+
+
+/*-------------------------------- OR -----------------------------------*/
+
+
+
+#include <iostream> // Example of a header file inclusion
+
+using namespace std ; //To avoid using std with every line of code like above example
+int main() {
+    // Body of the program
+    cout << "Hii, Welcome to AKHSS,Gilgit ." << endl; // Output a message
+    return 0; // Terminate the program
+}
+
