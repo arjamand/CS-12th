6d411cee12e9f843ac9266dc05596269aecc17b3
Arjamand Ali <arjamandali497@gmail.com>
2023-08-05 12:38:03 -0700

    adding the missing topic (the very first topic ) to the rep
    - Added code examples
    - Added code snippets
    - Created Readme file and included some definitions and added the code snippets to the readme file

---
+++ 000-intro-to-cpp/002-comments.cpp
@@ -0,0 +1,14 @@
+// 2023-08-05 12:29:06
+
+#include <iostream>
+using namespace std ;
+
+int main() {
+    // This is a single-line comment
+    
+    /* This is a
+       multiple-line comment */
+       
+    cout << "Hello, world!" << endl; // Output a message
+    return 0;
+}
