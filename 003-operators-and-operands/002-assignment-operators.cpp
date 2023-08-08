3c925c9b601d146eb267df89d2dbe2019a8c367e
Arjamand Ali <arjamandali497@gmail.com>
2023-08-08 04:09:17 -0700

    Create 002-assignment-operators.cpp

---
+++ 003-operators-and-operands/002-assignment-operators.cpp
@@ -0,0 +1,14 @@
+// 2023-08-08 04:06:03
+
+#include <iostream>
+using namespace std ;
+
+int main(){
+    int num1 = 5 ;
+    int num2 = 10 ;
+
+    //assigning the value of num1 + num2 to nums3
+    int num3 = num1 + num2 ;
+    
+    cout << num3 ;
+}
