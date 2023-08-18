ee76157f044c6d58a7eca632c976459f901cf508
Arjamand Ali <arjamandali497@gmail.com>
2023-08-18 02:45:24 -0700

    Create 001-for-loops.cpp

---
+++ 005-loops/001-for-loops.cpp
@@ -0,0 +1,16 @@
+// 2023-08-18 02:43:40
+// write a for loop which prints out count 1-100
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+  for (int i = 0; i <= 100; i++)
+    {
+      cout << i << endl;
+    }
+
+  return 0;
+}
+
