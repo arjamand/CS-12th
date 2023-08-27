64a72bd330f1ecd7588dc9527d3ca22cbd97c421
Arjamand Ali <140328490+arjamand@users.noreply.github.com>
2023-08-27 02:48:50 -0700

    cpp class work and practice

---
+++ 006-intro-to-arrays/001-declare-initialize-arrays.cpp
@@ -0,0 +1,14 @@
+// 2023-08-27 01:11:27
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int intArray[5];          // Array of integers with size 5
+    double doubleArray[10];   // Array of doubles with size 10
+    char charArray[8];        // Array of characters with size 8
+
+    int intArray2[5] = {10, 25, 18, 42, 57};  // Initializing an integer array
+
+    return 0;
+}
