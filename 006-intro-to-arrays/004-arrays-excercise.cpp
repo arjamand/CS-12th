c6216acda23f66dbee5eaca7fc8d443e1d8d587b
Arjamand Ali <140328490+arjamand@users.noreply.github.com>
2023-08-31 06:25:45 -0700

    1D arrays Excercise and 2D arary start

---
+++ 006-intro-to-arrays/004-arrays-excercise.cpp
@@ -0,0 +1,21 @@
+// 2023-08-31 06:23:48
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int marks[8];
+    int total_marks = 800;
+    float obt_marks ;
+    obt_marks = 0;
+
+    for (int i = 0; i<8; i++  ){
+        cout << "Subject "<<(i + 1) <<" marks (out of 100) : " ;
+        cin >> marks[i]  ;
+        obt_marks += marks[i];
+        
+    }
+    
+    float percent = (obt_marks/total_marks)*100;
+    cout<<endl<<"The Percentage obtained is "<<percent<<"%";
+}
