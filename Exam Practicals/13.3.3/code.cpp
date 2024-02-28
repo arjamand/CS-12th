9aee41896c42ca0fbe115ce24b95fd5f29491018
Arjamand Ali <140328490+arjamand@users.noreply.github.com>
2024-02-27 19:43:07 -0800

    completed 13.3.3

--- Exam Practicals/13.3.3/code.cpp
+++ Exam Practicals/13.3.3/code.cpp
@@ -1,3 +1,72 @@
-// 22/02/2024 04:47 PM
+// 2024-02-27 05:02:35
 /* 13.3.3 Write C++ program to perform various operations on string using string fucntions , 
 i.e. strcpy, strcat, strlen and strcmp */
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main(){
+
+    // strcpy()
+    char destination[] = "" ;
+    char source[] = "arjamand ali";
+
+    cout << "\nTesting copying string from variable to variable 'strcpy()'.\n";
+    cout << "Destination string : "<< destination << "\nSource string : "<< source ;
+
+    //copying string from source variable to destination variable
+    strcpy(destination, source);
+
+    cout << "\n\nDestination string  : "<< destination << "\nSource string : "<< source <<endl;
+
+
+    //strcat()
+    char first_name[] = "Arjamand " ;
+    char last_name[] = "Ali" ;
+
+    cout << "\nTesting concatenating two variables with 'strcat()'.\n";
+    cout << "First name string  : "<< first_name << "\nLast name string string : "<< last_name ;
+
+    // concatenating last_name to first_name
+    strcat(first_name, last_name) ;
+
+    cout << "\n\nFirst name string  : "<< first_name << "\nLast name string string : "<< last_name ;
+
+
+    // strlen()
+    char name[] = "Arjamand" ;
+    
+    cout << "\n\nFinding lenght of a string with  'strlen()'.\n";
+
+
+    // finding length of the string 
+    int lenght = strlen(name);
+
+    cout << "The lenght of the string "<< name << " is "<< lenght ;
+
+    // strcmp()
+
+    char str_1 [] = "Arjamand" ;
+    char str_2 [] = "Ali";
+
+    cout << "\n\nComparing 'str_1 = Arjamand' and 'str_2 = Ali' wth strcmp() .\n";
+    // comparing str 1 and str 2 
+    int result = strcmp(str_1, str_2);
+
+    if (result == 0){
+        cout << "The strings are of equal length .";
+
+    }
+    else if (result < 0){
+        cout << "The lenght of str1 is smaller then str2 .";
+
+    }
+    else {
+        cout << "The lenght of str1 larger than str2 . ";
+    }
+
+
+    
+}
---
+++ Exam Practicals/14.1.7/code.cpp
@@ -0,0 +1,28 @@
+// 2024-02-27 02:39:02
+/*
+14.1.7 Write a program involving use of user defined function to calculate volume of 
+cylinder, sphere and cube.
+*/
+
+float VolumeOfCylinder(){
+
+
+}
+
+float VolumeOfSphere(){
+
+
+}
+
+float VolumeOfCube(){
+
+
+}
+
+int main (){
+
+    
+
+
+    return 0 ;
+}
